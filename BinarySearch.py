878e2b2b5099d454e2639813cc9cb499fcfae5f9
Niki <nikip9795@gmail.com>
2024-04-08 16:30:41 -0400

    added 2D binary search

--- BinarySearch.py
+++ BinarySearch.py
@@ -1,11 +1,14 @@
-class BinarySearch():
+"""
+Binary Search on 1D array
+"""
+class BinarySearch1D():
 
     def __init__(self, array, target):
         self.array = sorted(self.array)
         print("Sorted array is:", self.array)
         self.target = target
         
-    def binarySearch(self):
+    def binarySearch1D(self):
         start = 0
         end = len(self.array) - 1
         
@@ -25,5 +28,38 @@ class BinarySearch():
     
 array = [int(n) for n in input("Enter the elements of an array seperated by comma\n").split(",") if n.strip()]
 target = int(input("Enter an element to search in an array\n"))
-binarysearch = BinarySearch(array, target)
-binarysearch.binarySearch()
+binarysearch1D = BinarySearch1D(array, target)
+binarysearch1D.binarySearch1D()
+
+"""
+Binary Search on 2D array
+"""
+class BinarySearch2D():
+
+    def __init__(self, array, target):
+        self.array = array
+        self.target = target
+
+    def binarySearch2D(self):
+
+        for i in range(len(self.array)):
+            for j in range(len(self.array)):
+                if self.array[i][j] == self.target:
+                    return f"Target found in given array at index array[{i},{j}]"
+        return f"Target does not found in given array"
+
+# Taking input for a 2D array
+rows = int(input("Enter the number of rows: "))
+cols = int(input("Enter the number of columns: "))
+
+print("Enter elements of the array row-wise, separated by commas:")
+
+array = []
+for _ in range(rows):
+    row = [int(n) for n in input().split(",") if n.strip()]
+    array.append(row)
+
+target = int(input("Enter an element to search in the array: "))
+
+binarysearch2D = BinarySearch2D(array, target)
+print(binarysearch2D.binarySearch2D())
