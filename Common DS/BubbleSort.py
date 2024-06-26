99949eb826fbff51f1b91214a98b669274fcdcf7
Niki <nikip9795@gmail.com>
2024-06-26 12:04:15 -0400

    adding Bubble Sort

---
+++ Common DS/BubbleSort.py
@@ -0,0 +1,25 @@
+class BubbleSort():
+
+    def __init__(self, array):
+        self.array = array
+        self.length = len(array)
+
+    def bubbleSort(self):
+        swapperd = False
+        for i in range(self.length - 1):
+            for j in range(1, self.length - i):
+                if self.array[j - 1] > self.array[j]:
+                    temp = self.array[j - 1]
+                    self.array[j - 1] = self.array[j]
+                    self.array[j] = temp
+                    j += 1
+                    swapperd = True
+            i += 1
+
+            if not swapperd:
+                break 
+        return self.array
+    
+array = [int(n) for n in input("Enter the elements of an array seperated by comma\n").split(",") if n.strip()]
+bubble = BubbleSort(array)
+print(bubble.bubbleSort())
