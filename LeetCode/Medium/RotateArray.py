bbc6f99c352dcc94fe5e46538deb9396b7758098
Niki <nikip9795@gmail.com>
2024-07-02 14:46:56 -0400

    adding more problems

---
+++ LeetCode/Medium/RotateArray.py
@@ -0,0 +1,23 @@
+class Solution():
+
+    def rotate(self, nums, k):
+
+        k = k % len(nums)
+
+        l, r = 0, len(nums) - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l + 1, r - 1
+
+        l, r = 0, k - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l + 1, r - 1
+
+        l, r = k, len(nums) - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l + 1, r - 1
+
+solution = Solution()
+print(solution.rotate([-1,-100,3,99], 2))
