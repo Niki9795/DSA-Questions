0ead22f88aeb09282fad6da0c4af0600774e2b79
Niki <nikip9795@gmail.com>
2024-08-20 15:24:01 -0400

    adding purmutation for duplicate numbers

--- Common DS/Permutations.py
+++ Common DS/Permutations.py
@@ -39,3 +39,29 @@ class Permutations():
 array = [str(n) for n in input("Enter the elements of an array seperated by comma\n").split(",") if n.strip()]
 permutations = Permutations()
 print("Possible Permutations are:", permutations.permutations(array))
+
+"""
+Permutation of duplicate elements using Iteration
+"""
+class Permutations():
+    
+    def permutations(self, array):
+        outerResult = [[]]
+
+        start = 0
+        end = 0
+        for i in range(len(array)):
+            if i > 0 and array[i] == array[i - 1]:
+                start = end + 1
+            end = len(outerResult) - 1
+            arraySize = len(outerResult)
+            for j in range(start, arraySize):
+                innerResult = [] + outerResult[j]
+                innerResult.append(array[i])
+                outerResult.append(innerResult)
+
+        return outerResult
+    
+array = [str(n) for n in input("Enter the elements of an array seperated by comma\n").split(",") if n.strip()]
+permutations = Permutations()
+print("Possible Permutations are:", permutations.permutations(sorted(array)))
